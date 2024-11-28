32727d6ffe319b34731433680cf7a6299647d876
ubermensch <samratmetaladhikari@gmail.com>
2024-11-28 20:41:56 +0545

    Update index.tsx

--- frontend/src/screens/home/index.tsx
+++ frontend/src/screens/home/index.tsx
@@ -225,7 +225,7 @@ export default function Home() {
                     variant="default"
                     color="black"
                 >
-                    Reset
+                    Clear
                 </Button>
 
                 {/* Calculate Button */}
